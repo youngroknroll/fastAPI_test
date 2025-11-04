ee6aafb79c211ea3fa4d82de887d5a66d86a9302
Yeongrok Song <song7868@gmail.com>
2025-11-04 23:42:21 +0900

    delete all

--- app/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from app.api.routers import article_router
-app = FastAPI()
-
-app.include_router(article_router.router, prefix="/articles", tags=["articles"])
--- tests/test_article_api.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from httpx import AsyncClient, ASGITransport
-from app.main import app
-
-#
-pytestmark = pytest.mark.anyio("asyncio")
-
